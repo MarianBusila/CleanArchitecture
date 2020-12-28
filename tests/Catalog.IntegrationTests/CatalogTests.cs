feba2388a09ee2de2386064c9f1cf9452d41cc96
Marian Busila <mbusila@pfdrive.com>
2020-12-28 01:41:37 -0500

    add a first integration test

---
+++ tests/Catalog.IntegrationTests/CatalogTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Catalog.Api;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Catalog.IntegrationTests
+{
+    public class CatalogTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CatalogTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+        }
+
+        [Fact]
+        public async Task GetPlaylists_should_return_ok()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            HttpResponseMessage response = await client.GetAsync("v1/playlists");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+    }
+}
